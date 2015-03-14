9c6cb725b63a6b5db057ca457c3685928f7f1ab9
Markus Mayer <development@widemeadows.de>
2015-03-14 04:22:58 +0100

    now always storing estimates, regardless of observations, in order to visualize performance

--- sine-match/sinematch.m
+++ sine-match/sinematch.m
@@ -21,15 +21,15 @@ estimated_data = nan(size(real_data));
 
 % plot the real data
 close all;
-plot(time_vector, real_data);
+plot(time_vector, real_data, 'k');
 xlabel('t [s]');
 ylabel('a*sin(\omegat+\phi)+b');
 
 % "it it is easier to approximate
 %  a probability distribution than it is to approximate
 %  an arbitrary nonlinear function or transformation"
-% J. K. Uhlmann, ï¿½Simultaneous map building and localization for
-% real time applications,ï¿½ transfer thesis, Univ. Oxford, Oxford, U.K.,
+% J. K. Uhlmann, “Simultaneous map building and localization for
+% real time applications,” transfer thesis, Univ. Oxford, Oxford, U.K.,
 % 1994.
 
 % set initial state estimate
@@ -39,17 +39,17 @@ x = [1;  % frequency [Hz]
      0]; % offset [<scalar>]
  
 % set initial state covariance
-P = 1000*diag(ones(size(x)));
+P = 100*diag(ones(size(x)));
  
 % define additive state covariance prediction noise
-R = 0 * ...
-    [1 0 0   0;     % frequency may change over time
-     0 1 0   0;     % phase may change over time
-     0 0 0.01 0;     % amplitude does not change
-     0 0 0   0.01];  % offset does not change
+R = 1 * ...
+    [1 0 0    0;      % frequency
+     0 1 0    0;      % phase
+     0 0 .01  0;      % amplitude
+     0 0 0    0.01];  % offset
 
 % define additive measurement covariance prediction noise
-z_sigma = .2;
+z_sigma = 0.05;
 Q = ones(size(1))*z_sigma;
 
 % simulate
@@ -77,19 +77,20 @@ for i=1:numel(time_vector);
     % add prediction noise
     P_prior = P_prior + R;
     
+    % predict observations using the a-priori state
+    % Note that the weights calculated by this function are the very
+    % same as calculated above since we're still operating on 
+    % the state vector (i.e. dimensionality didn't change).
+    [z_estimate, S_estimate, Z] = unscented(observation_fun, ...
+                                            x_prior, P_prior, ...
+                                            'n_out', 1);
+    
     if mod(i,floor(10*rand(1))) ~= 0
         % pass variables around
+        estimated_data(i) = z_estimate;
         x = x_prior;
         P = P_prior;
     else
-        % predict observations using the a-priori state
-        % Note that the weights calculated by this function are the very
-        % same as calculated above since we're still operating on 
-        % the state vector (i.e. dimensionality didn't change).
-        [z_estimate, S_estimate, Z] = unscented(observation_fun, ...
-                                                x_prior, P_prior, ...
-                                                'n_out', 1);
-
         % add measurement noise Q to S_estimate
         S_estimate = S_estimate + Q;
 
@@ -111,7 +112,7 @@ for i=1:numel(time_vector);
         P_posterior = P_prior - K*S_estimate*K';
 
         % pass variables around
-        estimated_data(i) = z_estimate;
+        estimated_data(i) = observation_fun(x_posterior);
         observed_data(i) = z;
         x = x_posterior;
         P = P_posterior;
@@ -121,5 +122,5 @@ for i=1:numel(time_vector);
 % plot the estimated data
 hold all;
 valid = ~isnan(estimated_data);
-plot(time_vector(valid), estimated_data(valid), 'r', 'LineWidth', 2);
+plot(time_vector(valid), estimated_data(valid), 'r', 'LineWidth', 1);
 plot(time_vector(valid), observed_data(valid), 'm+');
