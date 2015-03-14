c56a54d7650c01d99d3a2a7371032f15a65d66bf
Markus Mayer <development@widemeadows.de>
2015-03-14 03:43:22 +0100

    sort-of-okay tracking

--- sine-match/sinematch.m
+++ sine-match/sinematch.m
@@ -4,12 +4,12 @@ real_amplitude = 2;           % <scalar>
 real_offset    = -5;          % <scalar>
 
 T_start        = 0;           % seconds
-T_end          = 10;          % seconds
-N_samples      = 1000;        % <number of samples>
+T_end          = 20;          % seconds
+N_samples      = 2000;        % <number of samples>
 
 % generate the time vector
 time_vector = linspace(T_start, T_end, N_samples);
-
+ 
 % generate the data vector
 real_omega = 2*pi*real_frequency;
 real_data = real_amplitude*sin(real_omega*time_vector+real_phase) + real_offset;
@@ -40,10 +40,15 @@ x = [1;  % frequency [Hz]
 P = 1000*diag(ones(size(x)));
  
 % define additive state covariance prediction noise
-R = ones(size(P))*0.1;
+R = (1)^2 * ...
+    [1 0 0   0;     % frequency may change over time
+     0 1 0   0;     % phase may change over time
+     0 0 0.01 0;     % amplitude does not change
+     0 0 0   0.01];  % offset does not change
 
 % define additive measurement covariance prediction noise
-Q = ones(size(1))*0.1;
+z_sigma = .2;
+Q = ones(size(1))*z_sigma;
 
 % simulate
 for i=1:numel(time_vector);
@@ -54,55 +59,63 @@ for i=1:numel(time_vector);
     else
         T = 0;
     end
-    
+        
     % define the nonlinear state transition function
     state_transition_fun = @(x) x;
 
     % define the nonlinear observation function
     % note this is time dependent
     observation_fun = @(x) x(3)*sin(2*pi*x(1)*t+x(2))+x(4);
-    
+   
     % time update - propagate state
     [x_prior, P_prior, X, Xwm, Xwc] = unscented(state_transition_fun, ...
                                                 x, P, ...
                                                 'n_out', 4);
-                                              
+                                            
     % add prediction noise
     P_prior = P_prior + R;
     
-    % predict observations using the a-priori state
-    % Note that the weights calculated by this function are the very
-    % same as calculated above since we're still operating on 
-    % the state vector (i.e. dimensionality didn't change).
-    [z_estimate, S_estimate, Z] = unscented(observation_fun, ...
-                                            x_prior, P_prior, ...
-                                            'n_out', 1);
-    
-    % add measurement noise Q to S_estimate
-    S_estimate = S_estimate + Q;
+    if mod(i,5) ~= 0
+        % pass variables around
+        x = x_prior;
+        P = P_prior;
+    else
+        % predict observations using the a-priori state
+        % Note that the weights calculated by this function are the very
+        % same as calculated above since we're still operating on 
+        % the state vector (i.e. dimensionality didn't change).
+        [z_estimate, S_estimate, Z] = unscented(observation_fun, ...
+                                                x_prior, P_prior, ...
+                                                'n_out', 1);
 
-    % calculate state-observation cross-covariance
-    Pxy = zeros(numel(x), numel(z_estimate));
-    for j=1:numel(Xwc)
-        Pxy = Pxy + Xwc(j)*(X(:,j)-x_prior)*(Z(:,j)-z_estimate)';
+        % add measurement noise Q to S_estimate
+        S_estimate = S_estimate + Q;
+
+        % calculate state-observation cross-covariance
+        Pxy = zeros(numel(x), numel(z_estimate));
+        for j=1:numel(Xwc)
+            Pxy = Pxy + Xwc(j)*(X(:,j)-x_prior)*(Z(:,j)-z_estimate)';
+        end
+
+        % calculate Kalman gain
+        K = Pxy/S_estimate; % note the inversion of S!
+
+        % obtain observation
+        z_error = z_sigma*randn(1);
+        z = real_data(i) + z_error;
+
+        % measurement update
+        x_posterior = x_prior + K*(z - z_estimate);
+        P_posterior = P_prior - K*S_estimate*K';
+
+        % pass variables around
+        estimated_data(i) = z_estimate;
+        x = x_posterior;
+        P = P_posterior;
     end
-    
-    % calculate Kalman gain
-    K = Pxy/S_estimate; % note the inversion of S!
-    
-    % obtain observation
-    z = real_data(i); % ... easy way out, should use more fake data here
-    
-    % measurement update
-    x_posterior = x_prior + K*(z - z_estimate);
-    P_posterior = P_prior - K*S_estimate*K';
-    
-    % pass variables around
-    estimated_data(i) = z_estimate;
-    x = x_posterior;
-    P = P_posterior;
  end
 
 % plot the estimated data
 hold all;
-plot(time_vector, estimated_data, 'r');
+valid = ~isnan(estimated_data);
+plot(time_vector(valid), estimated_data(valid), 'r');
